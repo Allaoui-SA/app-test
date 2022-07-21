6794a63cb966d2b81854a973f5f061d8195b11db
AllaouiSA <35000745+elthosan@users.noreply.github.com>
2022-07-21 10:45:57 +0200

    ajout de commentaires dans PrivateRoute.js

--- festival/src/components/PrivateRoute.js
+++ festival/src/components/PrivateRoute.js
@@ -5,26 +5,27 @@ import Login from './Login'
 
 export default function PrivateRoute({ path, children }) {
 
-  const { currentUser } = useAuth()
-  
-console.log('currentUser', currentUser)
+const { currentUser } = useAuth()
 
+  // Si un utilisateur est connecté
   if (currentUser) {
+    
+    // Si l'url chemine vers la vue Login ou Signup 
     if (path === '/login' || path === '/signup') {
+      // rediriger vers la home/dashboard
       return <Navigate to="/" />
-    } else {
-      return children
-    }
-  } else if (path === '/login' || path === '/signup') {
+    }    
+    // Sinon retouner l'enfant du composant PrivateRoute
+    return children
+    
+    // Fin de condition Connecté et page Login ou Signup
+  }
+
+  // Sinon si aucun utilisateur n'est connecté et que l'url chemine vers Login ou Signup
+  else if (path === '/login' || path === '/signup') {
+    // Retouner l'enfant du composant PrivateRoute
     return children
   }
-  
+  // Rediriger vers la page Login
   return <Navigate to="/login" />
-  
-//   if(currentUser) {
-//     if(children !== <Login />) {
-    
-//       return children
-//     } else return <Navigate to="/" />
-//   } else return <Navigate to="/login" />
 }
